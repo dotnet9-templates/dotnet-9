893c6d0a90b8860bddff5d369fb447d39fe6a9a5
Jacob Reola <jacobreola@gmail.com>
2025-12-23 22:30:58 -0600

    fixed spacing for end of part 8

--- Domain/Reactivity.cs
+++ Domain/Reactivity.cs
@@ -17,7 +17,6 @@ namespace Domain
         // location props
         public required string City { get; set; }
         public required string Venue { get; set; }
-        
         public double Latitude { get; set; }
         public double Longitude { get; set; }
     }
