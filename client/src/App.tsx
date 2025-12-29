686cedab40126a38b93e37d6d29f65e307331eeb
Jacob Reola <jacobreola@gmail.com>
2025-12-29 05:57:49 -0600

    end s319

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <h3>Dotnet-9</h3>;
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5001/api/activities")
+      .then((response) => response.json())
+      .then((data) => setActivities(data));
+  }, []);
+
+  return (
+    <div>
+      <h3 className="app" style={{ color: "red" }}>
+        Dotnet-9
+      </h3>
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
